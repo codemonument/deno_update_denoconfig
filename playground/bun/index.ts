6d300d4d9f903f1d396f46fd82e2739a3bd9ca7b
Benjamin Jesuiter <bjesuiter@gmail.com>
2024-03-29 23:51:13 +0100

    test failed: cannot run in bun (since the "Deno." namespace does not get polyfilled by jsr :O

--- playground/bun/index.ts
+++ playground/bun/index.ts
@@ -1,1 +1,8 @@
-console.log("Hello via Bun!");
+import { updateConfig } from "@codemonument/update-denoconfig";
+
+await updateConfig("test/demo/deno.jsonc", {
+  version: "1.0.0",
+  tasks: {
+    echo: "echo 'Hello World!'",
+  },
+});
