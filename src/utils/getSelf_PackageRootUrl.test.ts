e58a4f3f04323e87e087644d281f7f7b6ac2be80
Benjamin Jesuiter <bjesuiter@gmail.com>
2024-03-29 19:00:16 +0100

    improve naming for getSelf_PackageRootUrl

--- src/utils/getSelf_PackageRootUrl.test.ts
+++ src/utils/getSelf_PackageRootUrl.test.ts
@@ -1,10 +1,10 @@
 import { assert, assertEquals } from "@std/assert";
 import { describe, it } from "@std/testing/bdd";
-import { getSelfPackageRootUrl } from "./getSelfPackageRootUrl.ts";
+import { getSelf_PackageRootUrl } from "./getSelf_PackageRootUrl.ts";
 
-describe("getSelfPackageRootUrl", () => {
+describe("getSelf_PackageRootUrl", () => {
   it("should return the root of this package", () => {
-    const root = getSelfPackageRootUrl();
+    const root = getSelf_PackageRootUrl();
     console.log("Self Package Root:", root.href);
     assertEquals(root.protocol, "file:");
     assert(root.pathname.endsWith("deno_update_denoconfig"));
--- src/utils/getSelf_PackageRootUrl.ts
+++ src/utils/getSelf_PackageRootUrl.ts
@@ -3,7 +3,7 @@ import { dirname, join, normalize } from "@std/url";
 /**
  * @returns The root url of this package relative to this file.
  */
-export function getSelfPackageRootUrl() {
+export function getSelf_PackageRootUrl() {
   const thisFileUrl = new URL(import.meta.url);
   const thisPackageRoot = normalize(join(dirname(thisFileUrl), "..", ".."));
   return thisPackageRoot;
