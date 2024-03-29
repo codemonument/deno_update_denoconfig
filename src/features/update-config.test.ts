b89f80ea5cd08fa0a36c015adedc948b2e1a61ed
Benjamin Jesuiter <bjesuiter@gmail.com>
2024-03-29 19:34:15 +0100

    add test for should update the config file (with comments)

--- src/features/update-config.test.ts
+++ src/features/update-config.test.ts
@@ -1,14 +1,16 @@
-import { assertNotEquals } from "@std/assert";
+import { assertNotEquals, assertThrows } from "@std/assert";
 import { parse } from "@std/jsonc";
 import { describe, it } from "@std/testing/bdd";
+import { assertSnapshot } from "@std/testing/snapshot";
 import { join } from "@std/url";
+import * as json5 from "json5-writer";
 
 import { getSelf_PackageRootUrl } from "@/src/utils/getSelf_PackageRootUrl.ts";
 import { assertEquals } from "jsr:@std/assert";
 import { updateConfig } from "./update-config.ts";
 
 describe("updateConfig", () => {
-  it("should update the config file", async () => {
+  it("should update the config file (without comments)", async () => {
     const testfilePath = join(
       getSelf_PackageRootUrl(),
       "test/update-config/deno.jsonc",
@@ -33,6 +35,44 @@ describe("updateConfig", () => {
     await Deno.writeTextFile(
       testfilePath,
       JSON.stringify(initialConfig, undefined, 2),
+    );
+  });
+
+  it("should update the config file (with comments)", async (t) => {
+    const testfilePath = join(
+      getSelf_PackageRootUrl(),
+      "test/update-config-with-comments/deno.jsonc",
+    );
+
+    const initialTestfileContent = await Deno.readTextFile(testfilePath);
+
+    const initialJsoncParse = parse(initialTestfileContent);
+    const initialJson5Parse = json5.load(initialTestfileContent);
+    const updateObject = {
+      version: "1.0.0",
+      tasks: {
+        echo: "echo 'Hello World!'",
+      },
+    };
+
+    // Ensure that initialJsoncParse and updateObject are not the same
+    assertNotEquals(initialJsoncParse, updateObject);
+
+    await updateConfig(testfilePath, updateObject);
+    const updatedJson5Parse = json5.load(await Deno.readTextFile(testfilePath));
+
+    // compare the ast of the updated config to the saved snapshot
+    // should always match since this test function is deterministic, as long as the testfile and this test do not change.
+    await assertSnapshot(t, updatedJson5Parse.ast);
+
+    // restore initial state of testfile for next test
+    await Deno.writeTextFile(
+      testfilePath,
+      initialJson5Parse.toSource({
+        quote: "double",
+        trailingComma: false,
+        quoteKeys: true,
+      }),
     );
   });
 });
