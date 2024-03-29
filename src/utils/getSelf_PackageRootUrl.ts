f9b519711c35addf1a998fcc3474411eeeca3aaa
Benjamin Jesuiter <bjesuiter@gmail.com>
2024-03-29 19:01:57 +0100

    improve commenting

--- src/utils/getSelf_PackageRootUrl.ts
+++ src/utils/getSelf_PackageRootUrl.ts
@@ -4,6 +4,7 @@ import { dirname, join, normalize } from "@std/url";
  * @returns The root url of this package relative to this file.
  */
 export function getSelf_PackageRootUrl() {
+  // import.meta.url is the exact url to this getSelf_PackageRootUrl.ts file
   const thisFileUrl = new URL(import.meta.url);
   const thisPackageRoot = normalize(join(dirname(thisFileUrl), "..", ".."));
   return thisPackageRoot;
